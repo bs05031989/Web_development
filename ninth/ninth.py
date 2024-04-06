83108bc2abdb2abd81a812f3cebd6b55b2f4ad4b
Bhupender Sharma <sharma05031989@gmail.com>
2024-04-06 23:29:30 +0530

    ID & Classes

---
+++ ninth/ninth.py
@@ -0,0 +1,27 @@
+"""
+ID : Unique identifier
+
+Classes : Can be assigned to multiple components
+
+Multiple classes 
+
+id & classes are attributes of elements
+
+to access classes : .red
+to access id : #first
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
