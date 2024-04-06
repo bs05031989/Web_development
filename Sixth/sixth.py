b61c9386c658a6d388ecef3b1f5ccd48dbb11cd7
Bhupender Sharma <sharma05031989@gmail.com>
2024-04-06 22:19:57 +0530

    SEO Core Web Vitals Lighthouse Report Meta tags

---
+++ Sixth/sixth.py
@@ -0,0 +1,25 @@
+"""
+SEO & Core WEB Vitals
+
+Search engine optimization
+
+Cumulative Layout Shift
+
+Largest Contextual Paint Best : 2.5 Sec
+
+FID : First input display : User first interact with a page. Best 100 milli Secs
+
+Lighthouse : A report tolds about web vitals
+
+Meta tags : Title : Must reflect the content
+
+Meta keywords not parsed now a days
+
+Reference : Web.dev more about Lighthouse report contextual meaning.
+
+
+
+
+
+
+"""
