5ba4f80b0b9f6611a0ea63d16b51f32322f83754
Bhupender Sharma <sharma05031989@gmail.com>
2024-04-06 15:13:19 +0530

    Basic Functionality of Web Development Discussed

---
+++ one.py
@@ -0,0 +1,26 @@
+"""
+Zero to Mastery
+Frontend, backend, Database
+
+Google chrome browser 
+
+VS Code Browser : code editor
+
+Client 
+
+Browser : Assistant to serve client
+
+
+Server 
+
+Client browser server
+
+Backend : How to process data to provide response in browser.
+
+Frontend: HTML,CSS,JS
+
+HTML : BODY Skelton
+CSS : Beautification
+JS : Working
+
+"""
