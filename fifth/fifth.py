3624e1e652f6625ab0588c519e9492786c4b24df
Bhupender Sharma <sharma05031989@gmail.com>
2024-04-06 17:49:38 +0530

    Images Table Unordered Ordered definition List

---
+++ fifth/fifth.py
@@ -0,0 +1,29 @@
+"""
+Images implemented by using image tag.
+
+<img src="image.png" alt="image">
+
+Table Implementation Details
+
+<table>
+</table>
+table row span code 
+
+Unordered list
+ordered list
+
+Definition List
+   dt
+   dd
+
+   
+
+
+
+ 
+
+
+
+
+
+"""
