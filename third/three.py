96d446fbcb220fb49adc1279fbe59c319edae587
Bhupender Sharma <sharma05031989@gmail.com>
2024-04-06 16:07:55 +0530

    HTML basic Tags & Live Preview connection

---
+++ third/three.py
@@ -0,0 +1,22 @@
+"""
+HTML code changes can be reflected in the live server without refresh
+
+CSS code changes to reflect you need to refresh a server.
+
+Two types of tag : Self closing tags
+                    pair tags
+
+Title tags : used for search engine optimization
+meta tag : for information
+
+
+Connection to phone using live preview server.
+
+
+
+
+
+
+
+
+"""
