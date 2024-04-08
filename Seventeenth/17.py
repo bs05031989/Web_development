238ac83a40e7435401caa41bfe407eeaebbd7df0
Bhupender Sharma <sharma05031989@gmail.com>
2024-04-08 18:38:02 +0530

    CSS Selectors

---
+++ Seventeenth/17.py
@@ -0,0 +1,24 @@
+"""
+Selectors
+
+Tag selector like div
+Inside selector like class
+
+Element Selector
+Class Selector
+ID selector
+Child Selector
+Descedent Selector
+Universal Selector
+Pseudo Selector
+
+
+
+
+
+
+
+
+
+
+"""
