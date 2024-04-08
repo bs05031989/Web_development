f24f94b94b36403c3a18513cab346c2219edd8e4
Bhupender Sharma <sharma05031989@gmail.com>
2024-04-08 16:32:07 +0530

    CSS Inline Internal External

---
+++ Fourteen&Fifteen/14.py
@@ -0,0 +1,22 @@
+"""
+Cascading style sheets
+
+Selectors
+ 
+<style>
+
+    div{color:#f00;font-size:20px;} Selector
+
+</style>
+
+There are three ways to incorporate css 
+1. Inline : using style tabs (Inside Tags)
+2. External : using external css file
+3. Internal : using internal style tabs( Explicit style tag)
+
+
+
+
+
+
+"""
